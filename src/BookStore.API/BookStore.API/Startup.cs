9f45dc4fecb93d1f0c18db62f7cfc2ae0858ad20
Ameya Shinde <ameyashinde123@outlook.com>
2022-05-12 00:44:50 +0530

    Created book controller and repository.

--- src/BookStore.API/BookStore.API/Startup.cs
+++ src/BookStore.API/BookStore.API/Startup.cs
@@ -1,3 +1,4 @@
+using BookStore.API.Repository;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -25,8 +26,8 @@ namespace BookStore.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllers();
+            services.AddTransient<IBookRepository, BookRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
